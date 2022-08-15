1af52bba5e0e18ed583ff2ae9b9fbcb1579c8445
amber <10846014@ntub.edu.tw>
2022-08-15 11:42:22 +0800

    invite

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,21 +1,23 @@
-<script>
+<script setup>
+import invitec from './invitec.vue';
 </script>
 
 <template>
-<div class="wrapper">
-    <div class="bookmark h-12 flex">
-        <div class="w-full flex text-center items-center">
-            <div class="main w-half px-4 bg-[CEE5F2] h-full flex items-center">北商大母親節活動</div>
-            <div class="budget w-half px-4 bg-zinc-300 h-full flex items-center">預算編輯</div>
+    <div class="wrapper">
+        <div class="bookmark h-12 flex">
+            <div class="w-full flex text-center items-center">
+                <div class="main w-half px-4 bg-[CEE5F2] h-full flex items-center">北商大母親節活動</div>
+                <div class="budget w-half px-4 bg-zinc-300 h-full flex items-center">預算編輯</div>
+            </div>
+            <div class="flex items-center pr-4">
+                <img src="/src/assets/images/home.svg" class="h-8">
+            </div>
         </div>
-        <div class="flex items-center pr-4">
-            <img src="/src/assets/images/home.svg" class="h-8">
+        <div class="content bg-[CEE5F2]">
+            <!-- 把你寫的Component放在這裡測試，要上github前這個檔案更動要discard掉-->
+           <invitec></invitec>
         </div>
     </div>
-    <div class="content bg-[CEE5F2]">
-        <!-- 把你寫的Component放在這裡測試，要上github前這個檔案更動要discard掉-->
-    </div>
-</div>
 </template>
 
 <style scoped>
@@ -26,7 +28,27 @@
 .content {
     height: calc(100% - 3rem)
 }
+
 .bg-\[CEE5F2\] {
     background-color: #CEE5F2;
+}
+
+.file-sec-wrapper {
+    position: fixed;
+    right: 25px;
+    bottom: 20px;
+    height: 40%;
+    background: #FFFFFF;
+    border: 1px solid rgba(0, 0, 0, 0.16);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 2px;
+}
+
+.file-shadow {
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.file>div>div>span {
+    display: block;
 }
 </style>
