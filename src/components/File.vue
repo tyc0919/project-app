c90d365bd47bfbbf50fcef72c86e73cca4490da9
tyc0919 <10846010@ntub.edu.tw>
2022-08-31 16:33:00 +0800

    Added Modal for File-User-Section
    - For Uploading and Inviting:
        - Defined two modes for each of them, this component expects a props named "mode" now, and it's for determining whether the coop users or file should be displayed
        - The array "data" needs to be implemented with axios requesting data from api in the future
    - For file deletion:
        - The modal is implemented in File.vue instead
        - File.vue will emit a event named "deleteFile", it will pass the file's id or name to the parent, and then the parent will send delete request to api

--- src/components/File.vue
+++ src/components/File.vue
@@ -1,4 +1,22 @@
 <script setup>
+import { ref } from 'vue';
+import Modal from './Modal.vue';
+
+const emit = defineEmits(['deleteFile'])
+const props = defineProps({
+    fname: String,
+    upload_time: String
+})
+
+const showModal = ref(false)
+const toggleModal = () => {
+    showModal.value = !showModal.value
+}
+const confirm = () => {
+    emit('deleteFile', 'id')
+    toggleModal()
+}
+
 </script>
 
 <template>
@@ -6,13 +24,30 @@
         <div class="flex">
             <img src="" alt="" class="w-12 h-12 inline mr-2">
             <div class="file-detail">
-                <span>檔案名稱.xlsx</span>
-                <span>上傳日期: 2022/07/20</span>
+                <!-- <span>檔案名稱.xlsx</span> -->
+                <span>{{props.fname}}</span>
+                <!-- <span>上傳日期: 2022/07/20</span> -->
+                <span>上傳日期: {{props.upload_time}}</span>
             </div>
         </div>
 
-        <button class="text-red-500">X</button>
+        <button class="text-red-500" @click="toggleModal">X</button>
     </div>
+    <Teleport to="body">
+            <!-- use the modal component, pass in the prop -->
+            <modal :show="showModal" @close="toggleModal()">
+                <template #header>
+                    <h3>確認刪除下列檔案?</h3>
+                </template>
+                <template #body>
+                    {{props.fname}}
+                </template>
+                <template #footer>
+                    <button class="bg-sky-700 text-white px-4 py-2 mx-1 rounded shadow-md" @click="confirm">確認</button>
+                    <button class="bg-gray-400 text-white px-4 py-2 mx-1 rounded shadow-md" @click="toggleModal">取消</button>
+                </template>
+            </modal>
+    </Teleport>
 </template>
 
 <style scoped>
