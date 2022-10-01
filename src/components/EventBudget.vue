4f6307f6c91ebd8a24fe47b3af70060796c9b5ce
BeDaShao <10846015@ntub.edu.tw>
2022-10-01 22:04:31 +0800

    Finish parts using GET
     - Remove all three component, but leave row html of those component at this file
     - But there still are  problems about POST needed to solve

--- src/components/EventBudget.vue
+++ src/components/EventBudget.vue
@@ -1,13 +1,123 @@
 <script setup>
+// moduals
 import { ref } from 'vue';
-import historyRecord from "./historyRecord.vue";
-import EventBudgetUpper from "./EventBudgetUpper.vue";
-import EventBudgetFile from './EventBudgetFile.vue';
+import { getCookie } from '../assets/modules'
+import axios from "axios";
+import { useRoute } from 'vue-router'
+// components
+import Modal from './Modal.vue';
+
+//api config 
+let csrftoken = getCookie('csrftoken')
+let config = {
+    headers: {
+        'X-CSRFToken': csrftoken
+    },
+    mode: 'same-origin'
+}
+const route = useRoute();
+const activityId = route.params.EventId
+
+
+
+// modal
 const showModal = ref(false)
 const toggleModal = () => {
     showModal.value = !showModal.value
 }
 const modalContent = ref('測試內容')
+
+// DATA
+// GET budget 
+let budgetData = ref("");
+let activityExpense = ref(0);
+axios.get('/api/activity/' + activityId + '/budget/', config)
+    .then(response => {
+        budgetData.value = response.data;
+        console.log("GET budget data");
+        console.log(response.data);
+
+        // total activity expense
+        for (let i = 0; i < budgetData.value.jobs.length; i++) {
+            activityExpense.value = budgetData.value.jobs[i].job_expenditure;
+        }
+
+        // append jobs data
+        for (let i = 0; i < budgetData.value.expenditures.length; i++) {
+            budgetData.value.expenditures[i].expenditure_uploaded_time = new Date(
+                budgetData.value.expenditures[i].expenditure_uploaded_time).toLocaleDateString();
+            for (let j = 0; j < budgetData.value.jobs.length; j++) {
+                if (budgetData.value.expenditures[i].job_serial_number ==
+                    budgetData.value.jobs[j].serial_number) {
+                    budgetData.value.expenditures[i]["job_title"] = budgetData.value.jobs[j].title;
+                    budgetData.value.expenditures[i]["person_in_charge_email"] = budgetData.value.jobs[j].person_in_charge_email;
+                }
+            }
+        }
+        console.log(budgetData.value);
+    });
+
+
+let budget = ref([]);
+async function getData() {
+    try {
+        axios.get('/api/activity/' + activityId + '/budget/', config)
+            .then(async function (response) {
+                // ? get userJobs in this activity.
+                budget.value = response.data;
+
+                let user = ref([]);
+                await axios.get('/api/userprofile/')
+                    .then(function (response) {
+                        user.value = response.data;
+                    });
+
+
+                let userJobs = ref([]);
+                for (let job of budget.value.jobs) {
+                    if (job.person_in_charge_email == user.value.user_email) {
+                        userJobs.value.push(job);
+                    }
+                }
+                budget.value["user_jobs"] = userJobs.value;
+
+                console.log("budget.value");
+                console.log(budget.value);
+            });
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+getData()
+
+// post
+let fileEl = ref(null);
+const expenditureUpload = () => {
+    let expenseEl = document.querySelector('#expense-el');
+    let jobEl = document.querySelector('#job-el');
+    let jobSerialNumber = null;
+    let activityId = null;
+
+    for (let job of budget.value.jobs) {
+        if (job.title == jobEl.value) {
+            jobSerialNumber = job.serial_number;
+        }
+    }
+
+    let formData = new FormData();
+    formData.append('file', fileEl.value.files[0]);
+    formData.append('serial_number', jobSerialNumber.value); //工作序號
+    formData.append('activity_id', activityId); //活動ID
+    formData.append('expense', expenseEl.value); //花費
+
+    axios.post('/api/upload/expenditure/', formData, config)
+        .then(function (response) {
+            console.log(response);
+        })
+}
+
+
+
 </script>
 
 <template>
@@ -19,16 +129,16 @@ const modalContent = ref('測試內容')
         <div class="flex justify-around mt-8 mb-16">
             <div class="bg-green-400 flex flex-col items-center p-2 rounded shadow-md">
                 <p class="m-2 text-center">預算</p>
-                <p class="mb-2 text-2xl w-56 text-center">$ 100,000</p>
+                <p class="mb-2 text-2xl w-56 text-center">$ {{budgetData.activity_budget}}</p>
             </div>
             <div class="bg-red-400 flex flex-col items-center p-2 rounded shadow-md">
                 <p class="m-2 text-center">支出</p>
-                <p class="mb-2 text-2xl w-56 text-center">$ 75,466</p>
+                <p class="mb-2 text-2xl w-56 text-center">$ {{activityExpense}}</p>
             </div>
         </div>
         <div class="flex justify-center w-full mb-8 ">
             <input type="number" class="w-1/3 mr-4" />
-            <button class="bg-sky-700 text-white px-4 py-2 rounded shadow-md" @click="toggleModal()">預算更新</button>
+            <button class="bg-sky-700 text-white px-4 py-2 rounded shadow-md">預算更新</button>
         </div>
 
         <!-- budget box end -->
@@ -37,17 +147,48 @@ const modalContent = ref('測試內容')
         <!-- test block start -->
         <Teleport to="body">
             <!-- use the modal component, pass in the prop -->
-            <modal :show="showModal" @close="toggleModal()">
+            <modal :show="showModal">
                 <template #header>
-                    <h3>自訂標題</h3>
+                    <div class="border-b-4 w-full px-4 py-4">
+                        <div class="font-bold text-2xl">上傳收據</div>
+                    </div>
+
                 </template>
+
                 <template #body>
-                    {{modalContent}}
+                    <div class="overflow-y-auto max-h-96 pr-4">
+                        <div class="flex-row justify-between space-y-3">
+
+                            <div class="text-base font-bold">支出金額</div>
+                            <div class="flex items-center justify-start space-x-3">
+                                <span class="italic font-bold">$</span>
+                                <input id="expense-el" type="number"
+                                    class="px-1 py-1 w-full text-base border border-2 border-slate-400"
+                                    placeholder="10000">
+                            </div>
+
+                            <div @change="expenditureUpload()" ref="fileEl" class="text-base font-bold">收據圖片證明</div>
+                            <input id="file-el" type="file">
+                            <div class="text-base font-bold ">所屬工作</div>
+                            <select id="job-el" class="px-1 py-1 w-full font-bold border border-2 border-slate-500">
+                                <option class="italic font-bold">--請選擇一個工作--</option>
+                                <option v-for="item in budget.user_jobs">{{item.title}}</option>
+                            </select>
+                        </div>
+                    </div>
                 </template>
+
                 <template #footer>
-                    <button class="bg-sky-700 text-white px-4 py-2 mx-1 rounded shadow-md">確認</button>
-                    <button class="bg-gray-400 text-white px-4 py-2 mx-1 rounded shadow-md"
-                        @click="toggleModal()">取消</button>
+                    <div class="border-t-2 pt-2">
+                        <button @click="[toggleModal(),expenditureUpload()]"
+                            class="btnComfirmCreateActivity mr-2 py-2 px-4 rounded text-green-500 border border-green-500 bg-transparent hover:text-white hover:bg-green-500 hover:font-semibold ">
+                            新增
+                        </button>
+                        <button @click="toggleModal()"
+                            class="btnCancelCreateActivity  py-2 px-4 rounded text-blue-500  bg-transparent  border border-blue-500 hover:text-white hover:bg-blue-500 hover:font-semibold ">
+                            取消
+                        </button>
+                    </div>
                 </template>
             </modal>
         </Teleport>
@@ -62,16 +203,29 @@ const modalContent = ref('測試內容')
             圖表放置處
         </canvas>
         <div class="w-1/2 pt-4 pb-4 bg-white h-96 rounded-lg shadow-md flex flex-col">
-            <button class="w-auto border-sky-700 border mx-8 mb-4 rounded text-sky-700"> 上傳</button>
+            <button @click="toggleModal()" class="w-auto border-sky-700 border mx-8 mb-4 rounded text-sky-700">
+                上傳</button>
             <div class="overflow-y-auto">
-                <EventBudgetFile />
-                <EventBudgetFile />
-                <EventBudgetFile />
-                <EventBudgetFile />
-                <EventBudgetFile />
-                <EventBudgetFile />
-                <EventBudgetFile />
-                <EventBudgetFile />
+                <div v-for="item in budgetData.expenditures"
+                    class="flex justify-between w-auto mt-4 mx-8 border-2 rounded-md py-2 pl-4 pr-2 border-gray-300 file-shadow">
+
+                    <div class="w-full whitespace-nowrap">
+                        <p class="font-bold">{{item.expenditure_receipt_path}}</p>
+                        <div class="w-full text-sm text-gray-500">
+                            <div>
+                                <span>上傳日期: </span>
+                                <span>{{item.expenditure_uploaded_time}}</span>
+                            </div>
+                            <div>
+                                <span>上傳者: </span>
+                                <span>{{item.person_in_charge_email}}</span>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <button class="text-red-500 p-2">X</button>
+                </div>
             </div>
 
         </div>
@@ -94,15 +248,17 @@ const modalContent = ref('測試內容')
             <div class="upload-time normal-square text-xl">上傳日期</div>
         </div>
         <hr>
-        <div class="normal-row">
+
+        <div class="normal-row" v-for="item in budgetData.expenditures">
             <div class="expense normal-square text-xl flex">
                 <div class="flex-1 text-left pl-10">$</div>
-                <div class="flex-2 pr-10 truncate">1235454</div>
+                <div class="flex-2 pr-10 truncate">{{item.expense}}</div>
             </div>
-            <div class="represent normal-square text-xl">陳莉莉</div>
-            <div class="work-name normal-square text-xl">場地安排</div>
-            <div class="upload-time normal-square text-xl">2011-08-12</div>
+            <div class="represent normal-square text-xl">{{item.person_in_charge_email}}</div>
+            <div class="work-name normal-square text-xl">{{item.job_title}}</div>
+            <div class="upload-time normal-square text-xl">{{item.expenditure_uploaded_time}}</div>
         </div>
+
         <div class="normal-row">
             <div class="expense normal-square text-xl flex">
                 <div class="flex-1 text-left pl-10">$</div>
@@ -112,24 +268,7 @@ const modalContent = ref('測試內容')
             <div class="work-name normal-square text-xl">交通</div>
             <div class="upload-time normal-square text-xl">2011-08-10</div>
         </div>
-        <div class="normal-row">
-            <div class="expense normal-square text-xl flex">
-                <div class="flex-1 text-left pl-10">$</div>
-                <div class="flex-2 pr-10 truncate">1235454</div>
-            </div>
-            <div class="represent normal-square text-xl">Alice</div>
-            <div class="work-name normal-square text-xl">food</div>
-            <div class="upload-time normal-square text-xl">2012-01-01</div>
-        </div>
-        <div class="normal-row">
-            <div class="expense normal-square text-xl flex">
-                <div class="flex-1 text-left pl-10">$</div>
-                <div class="flex-2 pr-10 truncate">1235454</div>
-            </div>
-            <div class="represent normal-square text-xl">Bob</div>
-            <div class="work-name normal-square text-xl">djiojei....</div>
-            <div class="upload-time normal-square text-xl">2008-09-08</div>
-        </div>
+
     </div>
 
 
