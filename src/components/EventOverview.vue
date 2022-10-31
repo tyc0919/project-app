bbae0c2f9276b12dd1330897f1598ac0dc10c487
BeDaShao <10846015@ntub.edu.tw>
2022-10-31 19:22:51 +0800

    Move logFile operations to the  file component

--- src/components/EventOverview.vue
+++ src/components/EventOverview.vue
@@ -8,72 +8,22 @@ import { useRoute } from 'vue-router'
 const route = useRoute()
 
 let logData = ref([])
-let logFile = ref([])
-
-let pages = ref([])
-const quantum = 5
-let pageNumber = ref(1)
-const changePage = (targetPage) => {
-    pageNumber.value = targetPage
-}
 
 const getData = async () => {
     await axios.get('/api/log/' + route.params.EventId + '/').then(function (response) {
         logData.value = response.data
     })
-    await axios.get('/api/file/activity/' + route.params.EventId + '/').then(function (response) {
-        logFile.value = response.data
-    })
-    sorter()
-}
-
-const sorter = () => {
-    pages.value = []
-    pageNumber.value = 1
-    let candidates = []
-    candidates = logFile.value
-
-    let selectBox = document.getElementById('select')
-    let selectInput = selectBox.options[selectBox.selectedIndex].value
-
-    if (selectInput == 'time') {
-        candidates.sort(function (a, b) {
-            return a.id - b.id
-        })
-    }
-    if (selectInput == 'job') {
-        candidates.sort(function (a, b) {
-            return a.job - b.job
-        })
-    }
-    if (selectInput == 'uploader') {
-        candidates.sort(function (a, b) {
-            return a.uploader - b.uploader
-        })
-    }
-
-    let pageData = []
-    for (let [index, data] of candidates.entries()) {
-        pageData.push(data)
-        if (pageData.length >= quantum || index == candidates.length - 1) {
-            pages.value.push(pageData)
-            pageData = []
-        }
-    }
 }
 getData()
 </script>
+
 <template>
     <div class="content">
         <!-- Log紀錄 -->
         <div class="border-b-2 border-[#c0c0c0]">
             <div class="w-full h-80 bg-white overflow-auto text-xl font-bold">
-                <EventOverviewLog
-                    v-for="log in logData"
-                    :time="log.time"
-                    :user="log.user_name"
-                    :action="log.action"
-                ></EventOverviewLog>
+                <EventOverviewLog v-for="log in logData" :time="log.time" :user="log.user_name" :action="log.action">
+                </EventOverviewLog>
             </div>
         </div>
         <!-- Log紀錄 -->
@@ -95,115 +45,11 @@ getData()
 
             <!-- 檔案區-->
             <div>
-                <EventOverviewFile
-                    v-for="file in pages[pageNumber - 1]"
-                    :key="file.id"
-                    :name="file.file_path"
-                    :date="file.file_uploaded_time"
-                    :uploader="file.uploader"
-                    :job-id="file.job"
-                ></EventOverviewFile>
+                <EventOverviewFile></EventOverviewFile>
             </div>
             <!-- 檔案區-->
 
-            <!--換頁-->
-            <div class="flex justify-center pb-10">
-                <nav aria-label="Page navigation example">
-                    <ul class="inline-flex -space-x-px text-xl shadow-primary">
-                        <li
-                            v-if="pageNumber - 1 > 0"
-                            @click="changePage(pageNumber - 1)"
-                            class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 ml-0 rounded-l-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            上一頁
-                        </li>
-                        <li
-                            v-else
-                            class="shadow-none text-opacity-30 bg-white border border-gray-300 text-gray-500 rounded-l-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
-                        >
-                            上一頁
-                        </li>
-                        <li
-                            v-if="pageNumber - 2 > 1"
-                            @click="changePage(1)"
-                            class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            1
-                        </li>
-                        <li
-                            v-if="pageNumber - 2 > 1"
-                            class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            ...
-                        </li>
 
-                        <li
-                            v-if="pageNumber - 2 >= 1"
-                            @click="changePage(pageNumber - 2)"
-                            class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            {{ pageNumber - 2 }}
-                        </li>
-                        <li
-                            v-if="pageNumber - 1 >= 1"
-                            @click="changePage(pageNumber - 1)"
-                            class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            {{ pageNumber - 1 }}
-                        </li>
-
-                        <li
-                            class="bg-blue-50 border border-gray-300 text-blue-600 hover:bg-blue-100 hover:text-blue-700 py-2 px-3 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-                        >
-                            {{ pageNumber }}
-                        </li>
-
-                        <li
-                            v-if="pageNumber + 1 <= pages.length"
-                            @click="changePage(pageNumber + 1)"
-                            class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            {{ pageNumber + 1 }}
-                        </li>
-                        <li
-                            v-if="pageNumber + 2 <= pages.length"
-                            @click="changePage(pageNumber + 2)"
-                            class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            {{ pageNumber + 2 }}
-                        </li>
-
-                        <li
-                            v-if="pageNumber + 2 < pages.length"
-                            class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            ...
-                        </li>
-                        <li
-                            v-if="pageNumber + 2 < pages.length"
-                            @click="changePage(pages.length)"
-                            class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            {{ pages.length }}
-                        </li>
-
-                        <li
-                            v-if="pageNumber < pages.length"
-                            @click="changePage(pageNumber + 1)"
-                            class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 rounded-r-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                        >
-                            下一頁
-                        </li>
-                        <li
-                            v-else
-                            class="shadow-none text-opacity-30 bg-white border border-gray-300 text-gray-500 rounded-r-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
-                        >
-                            下一頁
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-            <!--換頁-->
         </div>
 
         <!-- 檔案總覽 -->
@@ -215,11 +61,13 @@ getData()
     display: flex;
     justify-content: center;
 }
+
 .pagination ul {
     display: inline-flex;
     background-color: #fff;
     margin: 2rem 0;
 }
+
 .pagination ul li {
     text-align: center;
     padding: 10px 20px 10px 20px;
@@ -228,9 +76,11 @@ getData()
     margin-left: -1px;
     cursor: pointer;
 }
+
 .pagination ul li.dot {
     cursor: default;
 }
+
 .pagination ul li.current {
     background-color: #dbd8d4;
 }
--- src/components/EventOverviewFile.vue
+++ src/components/EventOverviewFile.vue
@@ -6,12 +6,6 @@ import Modal from './Modal.vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
-const props = defineProps({
-    name: String,
-    date: String,
-    uploader: String,
-    jobId: Number,
-})
 
 let csrftoken = getCookie()
 let config = {
@@ -21,8 +15,15 @@ let config = {
     mode: 'same-origin',
 }
 
-let jobName = ref('')
-let userName = ref('')
+// 分頁準備
+let pages = ref([])
+const quantum = 5
+let pageNumber = ref(1)
+const changePage = (targetPage) => {
+    pageNumber.value = targetPage
+}
+
+// 彈出視窗控制
 const modalController = {
     deleteModal: ref(false),
     deleteSuccessModal: ref(false),
@@ -30,46 +31,85 @@ const modalController = {
 const toggleModal = (modalName) => {
     modalController[modalName].value = !modalController[modalName].value
 }
-
+// 刪除檔案操作
+let selectedFile = ref(null)
+const setSelectedFileId = (file) => {
+    selectedFile.value = file
+}
 const deleteFile = async () => {
-    axios.post('/api/delete-file/', { model: 'file', job_id: props.jobId, file_name: props.name }, config)
+    let file = selectedFile.value
+    console.log(file)
+    await axios.post('/api/delete-file/', { model: 'file', job_id: file.job, file_name: file.file_path }, config)
+    getData()
     toggleModal('deleteModal')
     toggleModal('deleteSuccessModal')
 }
-
-const getJobName = async () => {
-    let job = []
-    let file = []
-    await axios.get('/api/activity/' + route.params.EventId + '/job/').then(async function (response) {
-        job = response.data
+// 取得檔案
+let logFile = ref([])
+const getData = async () => {
+    await axios.get('/api/file/activity/' + route.params.EventId + '/').then(function (response) {
+        logFile.value = response.data
     })
-    await axios.get('/api/file/job/' + props.jobId + '/').then(function (response) {
-        file = response.data
+    console.log(logFile.value)
+
+    // get users
+    let collaborators = []
+    await axios.get('/api/activity/' + route.params.EventId + '/collaborator/').then(function (response) {
+        collaborators = response.data
     })
 
-    file.forEach(function (f) {
-        job.forEach(function (j) {
-            if (f.job == j.id) {
-                jobName.value = j.title
+    // append some extra information
+    for (let file of logFile.value) {
+        // append user_name
+        for (let user of collaborators) {
+            if (file.uploader == user.user_email) {
+                file['user_name'] = user.user_name
             }
-        })
-    })
-}
-const getUserName = async () => {
-    let collab = []
-    await axios.get('/api/activity/' + route.params.EventId + '/collaborator/').then(async function (response) {
-        collab = response.data
-    })
-
-    collab.forEach(function (c) {
-        if (c.user_email == props.uploader) {
-            userName.value = c.user_name
         }
-    })
+        //append job_name
+        await axios.get('/api/activity/' + route.params.EventId + '/job/' + file.job + '/').then(function (response) {
+            file['job_name'] = response.data.title
+        })
+
+    }
 }
 
-getUserName()
-getJobName()
+// 檔案分頁
+const sorter = () => {
+    pages.value = []
+    pageNumber.value = 1
+    let candidates = []
+    candidates = logFile.value
+
+    let selectBox = document.getElementById('select')
+    let selectInput = selectBox.options[selectBox.selectedIndex].value
+
+    if (selectInput == 'time') {
+        candidates.sort(function (a, b) {
+            return a.id - b.id
+        })
+    }
+    if (selectInput == 'job') {
+        candidates.sort(function (a, b) {
+            return a.job - b.job
+        })
+    }
+    if (selectInput == 'uploader') {
+        candidates.sort(function (a, b) {
+            return a.uploader - b.uploader
+        })
+    }
+
+    let pageData = []
+    for (let [index, data] of candidates.entries()) {
+        pageData.push(data)
+        if (pageData.length >= quantum || index == candidates.length - 1) {
+            pages.value.push(pageData)
+            pageData = []
+        }
+    }
+}
+getData()
 </script>
 
 <template>
@@ -82,16 +122,12 @@ getJobName()
             </template>
             <template #body> 確定要刪除檔案嗎？此動作無法復原 </template>
             <template #footer>
-                <button
-                    @click="deleteFile()"
-                    class="mr-2 py-2 px-4 rounded text-green-500 border border-green-500 bg-transparent hover:text-white hover:bg-green-500 hover:font-semibold"
-                >
+                <button @click="deleteFile()"
+                    class="mr-2 py-2 px-4 rounded text-green-500 border border-green-500 bg-transparent hover:text-white hover:bg-green-500 hover:font-semibold">
                     確定
                 </button>
-                <button
-                    @click="toggleModal('deleteModal')"
-                    class="py-2 px-4 rounded text-blue-500 bg-transparent border border-blue-500 hover:text-white hover:bg-blue-500 hover:font-semibold"
-                >
+                <button @click="toggleModal('deleteModal')"
+                    class="py-2 px-4 rounded text-blue-500 bg-transparent border border-blue-500 hover:text-white hover:bg-blue-500 hover:font-semibold">
                     取消
                 </button>
             </template>
@@ -106,40 +142,105 @@ getJobName()
         </template>
         <template #body> 刪除成功！ </template>
         <template #footer>
-            <button
-                @click="toggleModal('deleteSuccessModal')"
-                class="mr-2 py-2 px-4 rounded text-green-500 border border-green-500 bg-transparent hover:text-white hover:bg-green-500 hover:font-semibold"
-            >
+            <button @click="toggleModal('deleteSuccessModal')"
+                class="mr-2 py-2 px-4 rounded text-green-500 border border-green-500 bg-transparent hover:text-white hover:bg-green-500 hover:font-semibold">
                 確定
             </button>
         </template>
     </Modal>
-    <div
-        class="flex justify-between w-auto my-4 py-4 pl-4 pr-2 border-2 border-[#3399FF] rounded-md bg-white drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]"
-    >
+    <div v-for="file in logFile"
+        class="flex justify-between w-auto my-4 py-4 pl-4 pr-2 border-2 border-[#3399FF] rounded-md bg-white drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
         <div class="w-full whitespace-nowrap">
-            <p class="font-bold">{{ props.name }}</p>
+            <p class="font-bold">{{ file.file_path }}</p>
             <div class="flex flex-row w-full mt-2 text-base text-gray-500 font-bold">
                 <div class="mr-2">
-                    <span>上傳日期： </span>
-                    <span>{{ props.date.substring(0, 10) }}</span>
+                    <span>上傳日期：{{ file.file_uploaded_time.substring(0, 10) }}</span>
+                    <span></span>
                 </div>
                 <div class="mx-2">
-                    <span>上傳時間： </span>
-                    <span>{{ props.date.substring(11, 19) }}</span>
+                    <span>上傳時間：{{ file.file_uploaded_time.substring(11, 19) }} </span>
+                    <span></span>
                 </div>
                 <div class="mx-2">
-                    <span>上傳者： </span>
-                    <span>{{ userName }}</span>
+                    <span>上傳者： {{ file.user_name }}</span>
+                    <span></span>
                 </div>
                 <div class="mx-2">
-                    <span>所屬工作： </span>
-                    <span>{{ jobName }}</span>
+                    <span>所屬工作： {{ file.job_name }}</span>
+                    <span></span>
                 </div>
             </div>
         </div>
-        <button class="text-red-500 p-2" @click="toggleModal('deleteModal')">X</button>
+        <button class="text-red-500 p-2" @click="[toggleModal('deleteModal'), setSelectedFileId(file)]">X</button>
     </div>
+
+    <!--換頁-->
+    <div class="flex justify-center pb-10">
+        <nav aria-label="Page navigation example">
+            <ul class="inline-flex -space-x-px text-xl shadow-primary">
+                <li v-if="pageNumber - 1 > 0" @click="changePage(pageNumber - 1)"
+                    class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 ml-0 rounded-l-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    上一頁
+                </li>
+                <li v-else
+                    class="shadow-none text-opacity-30 bg-white border border-gray-300 text-gray-500 rounded-l-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400">
+                    上一頁
+                </li>
+                <li v-if="pageNumber - 2 > 1" @click="changePage(1)"
+                    class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    1
+                </li>
+                <li v-if="pageNumber - 2 > 1"
+                    class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    ...
+                </li>
+
+                <li v-if="pageNumber - 2 >= 1" @click="changePage(pageNumber - 2)"
+                    class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    {{ pageNumber - 2 }}
+                </li>
+                <li v-if="pageNumber - 1 >= 1" @click="changePage(pageNumber - 1)"
+                    class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    {{ pageNumber - 1 }}
+                </li>
+
+                <li
+                    class="bg-blue-50 border border-gray-300 text-blue-600 hover:bg-blue-100 hover:text-blue-700 py-2 px-3 dark:border-gray-700 dark:bg-gray-700 dark:text-white">
+                    {{ pageNumber }}
+                </li>
+
+                <li v-if="pageNumber + 1 <= pages.length" @click="changePage(pageNumber + 1)"
+                    class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    {{ pageNumber + 1 }}
+                </li>
+                <li v-if="pageNumber + 2 <= pages.length" @click="changePage(pageNumber + 2)"
+                    class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    {{ pageNumber + 2 }}
+                </li>
+
+                <li v-if="pageNumber + 2 < pages.length"
+                    class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    ...
+                </li>
+                <li v-if="pageNumber + 2 < pages.length" @click="changePage(pages.length)"
+                    class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    {{ pages.length }}
+                </li>
+
+                <li v-if="pageNumber < pages.length" @click="changePage(pageNumber + 1)"
+                    class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 rounded-r-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    下一頁
+                </li>
+                <li v-else
+                    class="shadow-none text-opacity-30 bg-white border border-gray-300 text-gray-500 rounded-r-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400">
+                    下一頁
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <!--換頁-->
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
